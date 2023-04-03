8c00bbe4cda663852281126bd12362c4ec73dfc0
kwedwick <kwedwick@gmail.com>
2023-04-03 15:53:10 -0500

    super hero service complete. CRUD completed and EF added

--- BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
+++ BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorFullStackCrud.Client.Pages;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorFullStackCrud.Server.Controllers
@@ -7,32 +8,91 @@ namespace BlazorFullStackCrud.Server.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
-        public static List<Comic> comics = new List<Comic> { 
-            new Comic { Id = 1, Name = "Marvel" },
-            new Comic { Id = 2, Name = "DC" }
-        };
+        private readonly DataContext _context;
 
-        public static List<SuperHero> heroes = new List<SuperHero> {
-            new SuperHero { Id = 1, FirstName = "Peter", LastName = "Parker", HeroName = "Spiderman", Comic = comics[0] },
-            new SuperHero { Id = 2, FirstName = "Wanda", LastName = "Maximoff", HeroName = "Scarlet Witch", Comic = comics[0]},
-            new SuperHero { Id = 3, FirstName = "Bruce", LastName = "Wayne", HeroName = "Batman", Comic = comics[1] },
-        };
-
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
         {
+            var heroes = await _context.SuperHeroes.Include(hero => hero.Comic).ToListAsync();
             return Ok(heroes);
+        }
+
+        [HttpGet("comics")]
+        public async Task<ActionResult<List<Comic>>> GetComics()
+        {
+            var comics = await _context.Comics.ToListAsync();
+            return Ok(comics);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<SuperHero>> GetSingleHero(int id)
         {
-            var hero = heroes.FirstOrDefault(x => x.Id == id);
+            var hero = await _context.SuperHeroes
+                .Include(hero => hero.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
             if(hero == null)
             {
                 return NotFound("Sorry, hero not found.");
             }
             return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            hero.Comic = null;
+
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<SuperHero>> UpdateSuperHero(SuperHero hero, int id)
+        {
+            
+            var dbHero = await _context.SuperHeroes
+                .Include(h=> h.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
+            if(dbHero == null)
+            {
+                return NotFound("No hero was found :(");
+            }
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.HeroName = hero.HeroName;
+            dbHero.ComicId = hero.ComicId;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<SuperHero>> DeleteSuperHero(int id)
+        {
+
+            var dbHero = await _context.SuperHeroes
+                .Include(h => h.Comic)
+                .FirstOrDefaultAsync(h => h.Id == id);
+            if (dbHero == null)
+            {
+                return NotFound("No hero was found :(");
+            }
+            _context.SuperHeroes.Remove(dbHero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetDbHeroes());
+        }
+
+        private async Task<List<SuperHero>> GetDbHeroes()
+        {
+            return await _context.SuperHeroes.Include(hero => hero.Comic).ToListAsync();
         }
     }
 }
