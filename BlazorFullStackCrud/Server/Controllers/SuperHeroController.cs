71e92254cdc2a03bd697f69259a2bcbca50ba77d
kwedwick <kwedwick@gmail.com>
2023-04-03 10:16:05 -0500

    initial init. Created two classes and started the superhero controller

---
+++ BlazorFullStackCrud/Server/Controllers/SuperHeroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorFullStackCrud.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        public static List<Comic> comics = new List<Comic> { }; 
+    }
+}
